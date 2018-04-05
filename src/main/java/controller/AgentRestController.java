d7123e47171b0f1325abe840d03813c40df7acf0
uo251851 <marcialfco@live.com>
2018-04-05 12:16:17 +0100

    Added restController.

---
+++ src/main/java/controller/AgentRestController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import DBmanagement.impl.DBServiceClass;
+import agent.AgentLogin;
+
+@RestController
+public class AgentRestController {
+	
+	@Autowired
+	private DBServiceClass dbService;
+
+	@RequestMapping(value="/restLogin", method=RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<agent.AgentInfo> okUser(@RequestBody AgentLogin receivedInfo) {
+		agent.AgentInfo agent = dbService.getAgent(receivedInfo.getId(), receivedInfo.getPassword(), receivedInfo.getKind());
+		
+		if(agent == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		else return new ResponseEntity<>(agent, HttpStatus.OK);
+	}
+}
