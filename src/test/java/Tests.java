b8d23132c8189c8ed53ea0638e20129d00a091e3
PabloSuaGar <uo250924@uniovi.es>
2018-04-09 16:54:55 +0200

    Added mongo dependecy. Made the suite to erase the DB before the test execution through a MongoClient

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,3 +1,6 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -10,4 +13,11 @@ import org.junit.runners.Suite.SuiteClasses;
 	agent.MainControllerTest.class,
 	agent.ModelTest.class
 })
-public class Tests { }
+public class Tests {
+	@BeforeClass
+	public static void setUp(){
+		MongoClient mongo = new MongoClient(new MongoClientURI("mongodb://admin:EIIASW2018$@ds127888.mlab.com:27888/loader_i1a_db"));
+		mongo.getDatabase("loader_i1a_db").getCollection("loader_i1a_collection").drop();
+		mongo.close();
+	}
+}
